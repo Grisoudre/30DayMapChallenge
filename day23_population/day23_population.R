0383e974251314f1c3afe3004e266daaf5421c27
Cécile Rodrigues <cecile.rodrigues@protonmail.com>
2019-11-25 14:01:29 +0100

    Update day23_population.R

--- day23_population/day23_population.R
+++ day23_population/day23_population.R
@@ -11,24 +11,23 @@ pop <- read.csv2("data/base-ic-evol-struct-pop-2016.csv")
 shpNord <- st_read("data/shpNord.shp")
 
 # Mise en forme -----------
-pop <- pop %>% filter(DEP=="59")
-popd <- pop %>% group_by(COM) %>% summarise(M2ans = round(sum(P16_POP0002)/sum(P16_POP)*100,1),
+pop <- pop %>% filter(DEP=="59") %>% group_by(COM) %>% summarise(M2ans = round(sum(P16_POP0002)/sum(P16_POP)*100,1),
                                             P75ans = round(sum(P16_POP75P)/sum(P16_POP)*100,1),
                                             P65ans = round(sum(P16_POP65P)/sum(P16_POP)*100,1),
                                             pop = sum(P16_POP))
 
 # Fonds de carte -----------
 
-shpNord <- merge(shpNord, popd,
+shpNord <- merge(shpNord, pop,
                  by.x ="insee", by.y="COM", all.x=T)
 
-shpNordB <- cartogram_cont(shpNord,"pop" , itermax=4)
+shpNord <- cartogram_cont(shpNord,"pop" , itermax=4)
 
 ggplot() + 
-  geom_sf(data=shpNordB, aes(fill=P75ans), color ="transparent")+
+  geom_sf(data=shpNord, aes(fill=P75ans), color ="transparent")+
   #scale_fill_gradient(low='black',high="white")
    viridis::scale_fill_viridis(option ="E") +
-  coord_sf(crs = st_crs(shpNord), datum = NA)+
+  coord_sf( datum = NA)+
   theme(panel.grid.major = element_line(colour = 'transparent'),
         panel.background = element_rect("transparent"),
         title=element_text(size=16))+
